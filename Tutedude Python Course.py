13b1a37d4b20b4cbaddfa77a518c2531c5b138de
pahalbhavsar729-cpu <pahalbhavsar729@gmail.com>
2025-09-28 11:57:59 +0530

    initial commit

---
+++ Tutedude Python Course.py
@@ -0,0 +1,39 @@
+# Basic Hello World Program#
+print("hello World")
+
+#Basic Mathematical operation Program#
+x = 10 
+y = 2
+print(x/y)
+print(x+y)
+print(x-y)
+print(x*y)
+print(x**y)
+print(x**y/2)
+print(x/1-1)
+print(x)
+
+#Input function # 
+name = input("Enter Your name:")
+age = input("Enter Your age :")
+print("the age of" ,name, " is :" ,age)
+
+#Python program that takes two numbers as input and prints their sum.#
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the two number :"))
+print("The sum of two numbers is : ",num1 + num2)
+
+#python Program to Create a personalized greeting  #
+first_name = input("Enter the fisrt name of the user : ")
+last_name = input("Enter the last name of the user : ")
+full_name = print("The full name of the user is :",first_name + last_name)
+print("Hello !",first_name + last_name, "welcome to the party")
+
+#String Operations#
+String = "Pahal"
+print(String)
+print("Length of the String is : ",len(String))
+print(String.upper())
+print(String.lower())
+
+#Variables#
